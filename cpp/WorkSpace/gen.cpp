9d53607cf322893fb2bfccd06a61d5ca0b8d7855
carb0xylBase <carboxylbase123@gmail.com>
2025-10-28 16:45:24 +0800

    Z function

--- cpp/WorkSpace/gen.cpp
+++ cpp/WorkSpace/gen.cpp
@@ -1,31 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-    int T = 1;
-    int n = 5, m = 7;
-    cout<<T<<"\n";
-    for(int tc=0;tc<T;tc++){
-        vector<pair<int,int>> edges;
-        vector<int> p(n);
-        iota(p.begin(),p.end(),1);
-        shuffle(p.begin(),p.end(),rng);
-        for(int i=1;i<n;i++) edges.emplace_back(p[i-1],p[i]);
-        unordered_set<long long> seen;
-        auto key=[&](int a,int b){ if(a>b) swap(a,b); return (long long)a<<32 | (unsigned long long)b; };
-        for(auto &e:edges) seen.insert(key(e.first,e.second));
-        uniform_int_distribution<int> dist(1,n);
-        while((int)edges.size()<m){
-            int u=dist(rng), v=dist(rng);
-            if(u==v) continue;
-            long long k=key(u,v);
-            if(seen.count(k)) continue;
-            seen.insert(k);
-            edges.emplace_back(u,v);
-        }
-        cout<<n<<" "<<m<<"\n";
-        for(auto &e:edges) cout<<e.first<<" "<<e.second<<"\n";
+    freopen("input.txt", "w", stdout);
+    int n = 1000000;
+    cout << n << endl;
+    for (int i = 0;i<n;i++) {
+        cout << 2;
     }
 }
--- cpp/WorkSpace/main.cpp
+++ cpp/WorkSpace/main.cpp
@@ -13,12 +13,49 @@ using pll = pair<ll,ll>;
 using db = long double;
 using pdd = pair<db, db>;
 
-const ll N = 2000000;
+const ll N = 50000000;
 const ll INF = 5e18;
 const ll MOD = 1e9 + 7;
 
+namespace Z {
+    string s; 
+    int z[N];
+    void init() {
+        z[0] = 0;
+        int n = s.size();
+        for (int i = 1,l = 0,r = 0;i<n;i++) {
+            if (i <= r && z[i - l] < r - i + 1) {
+                z[i] = z[i - l];
+            } else {
+                z[i] = max(0, r - i + 1);
+                while (i + z[i] < n && s[z[i]] == s[i + z[i]]) ++ z[i];
+            }
+            if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
+        }
+        return;
+    }
+};
+using namespace Z;
+
 void solve() {
-    
+    string a, b;
+    cin >> a >> b;
+    s = b;
+    init();
+    ll ans = 0;
+    z[0] = b.size();
+    for (int i = 0;i<b.size();i++) {
+        ans ^= 1ll * (z[i] + 1) * (i + 1);
+    }
+    cout << ans << endl;
+    ans = 0;
+    s = b + "#" + a;
+    init();
+    vector<int> p(a.size(), 0);
+    for (int i = b.size()+1;i<s.size();i++) {
+        ans ^= 1ll * (z[i] + 1) * (i - b.size());
+    }
+    cout << ans << endl;
     return;
 }
 
@@ -32,7 +69,7 @@ signed main() {
     cin.tie(nullptr);
 
     int t = 1;
-    cin >> t;
+    // cin >> t;
 
     while (t--) {
         solve();
